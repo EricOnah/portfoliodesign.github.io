5076f0ced563bffcbad08a378689312da94d34f9
Eric Onah <ericdavid4u@gmail.com>
2023-04-03 21:37:34 +0100

    added DP resume and refactored js

--- app.js
+++ app.js
@@ -1,116 +1,81 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
-let toggleBtn = document.querySelector('.toggle-btn')
-let body = document.querySelector('html');
-let title = document.querySelectorAll('.section-title');
-let p = document.querySelectorAll('p');
-let cta = document.querySelector('.col-right .cta')
-let contactItem = document.querySelectorAll('.contact-item')
-let contactInfo = document.querySelectorAll('.contact-info')
-let darkMode = localStorage.getItem('darkMode');
+const select = (selector) => document.querySelector(selector);
+const selectAll = (selector) => document.querySelectorAll(selector);
 
+const hamburger = select(".header .nav-bar .nav-list .hamburger");
+const mobileMenu = select(".header .nav-bar .nav-list ul");
+const menuItem = selectAll(".header .nav-bar .nav-list ul li a");
+const header = select(".header.container");
+const toggleBtn = select(".toggle-btn");
+const body = document.querySelector("html");
+const title = selectAll(".section-title");
+const p = selectAll("p");
+const h2 = selectAll("h2");
+const cta = select(".col-right .cta");
+const contactItem = selectAll(".contact-item");
+const serviceItem = selectAll(".service-item");
+const contactInfo = selectAll(".contact-info");
+let darkMode = localStorage.getItem("darkMode");
 
+const enableDarkMode = () => {
+  toggleBtn.classList.add("active");
+  body.classList.add("active");
+  title.forEach((title) => title.classList.add("active"));
+  p.forEach((p) => p.classList.add("active"));
+  h2.forEach((h2) => h2.classList.add("active"));
+  contactItem.forEach((item) => item.classList.add("active"));
+  contactInfo.forEach((info) => info.classList.add("active"));
+  serviceItem.forEach((item) => item.classList.add("active"));
+  cta.classList.add("active");
+  localStorage.setItem("darkMode", "on");
+};
 
-// Dark mode start //
+const disableDarkMode = () => {
+  toggleBtn.classList.remove("active");
+  body.classList.remove("active");
+  title.forEach((title) => title.classList.remove("active"));
+  p.forEach((p) => p.classList.remove("active"));
+  h2.forEach((h2) => h2.classList.remove("active"));
+  contactItem.forEach((item) => item.classList.remove("active"));
+  contactInfo.forEach((info) => info.classList.remove("active"));
+  serviceItem.forEach((item) => item.classList.remove("active"));
+  cta.classList.remove("active");
+  localStorage.setItem("darkMode", "off");
+};
 
-      //darkMode enable start//
-	  darkModeEnable = () => {
-		toggleBtn.classList.add('active');
-    body.classList.add('active');
-	for (let i = 0; i < title.length; i++) {
-		title[i].classList.add('active')	
-	};
-	for (let i = 0; i < p.length; i++) {
-		p[i].classList.add('active')	
-	};
-	for (let i = 0; i < contactItem.length; i++) {
-		contactItem[i].classList.add('active')	
-	};
-	for (let i = 0; i < contactInfo.length; i++) {
-		contactInfo[i].classList.add('active')	
-	};
-	cta.classList.add('active')
-		
-	localStorage.setItem('darkMode', 'on')
-	  }
-
-	//********* **** dark mode enable end // ***********
-
-	  // dark mode disable start //
-	  darkModeDisable = () => {
-		toggleBtn.classList.remove('active');
-    body.classList.remove('active');
-	for (let i = 0; i < title.length; i++) {
-		title[i].classList.remove('active')	
-	};
-	for (let i = 0; i < p.length; i++) {
-		p[i].classList.remove('active')	
-	};
-	for (let i = 0; i < contactItem.length; i++) {
-		contactItem[i].classList.remove('active')	
-	};
-	for (let i = 0; i < contactInfo.length; i++) {
-		contactInfo[i].classList.remove('active')	
-	};
-	cta.classList.remove('active')
-		
-	localStorage.setItem('darkMode', 'off')
-	  }  
-	 
-	 
-	  // dark mode disable end //
-
-	// onload condition start //
-
-if (darkMode === 'on') {
-	darkModeEnable()
+if (darkMode === "on") {
+  enableDarkMode();
 }
 
-	// onload condition end //
-
-
-	//toggle btn click start //
-toggleBtn.addEventListener('click', () => {
-	darkMode = localStorage.getItem('darkMode');
-	if (darkMode !== 'on') {
-		darkModeEnable();
-	
-	} else {
-		darkModeDisable();
-	}
-})
-
-	//toggle btn click end //
-
-//**** **/ Dark mode end // **************
-
-
-
-// Hamburger menu start //
-
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+toggleBtn.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "on") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 50) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
 });
 
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+document.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+  if (scrollPosition > 50) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+  });
 });
 
 
 
-// Hamburger menu end //
+
