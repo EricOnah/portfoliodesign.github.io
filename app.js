56f9066b58470c4688fd5a9b804706f189c36bf3
Eric Onah <ericdavid4u@gmail.com>
2024-06-12 17:01:08 +0100

    updated portfolio

--- app.js
+++ app.js
@@ -1,116 +1,115 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
-let toggleBtn = document.querySelector('.toggle-btn')
-let body = document.querySelector('html');
-let title = document.querySelectorAll('.section-title');
-let p = document.querySelectorAll('p');
-let cta = document.querySelector('.col-right .cta')
-let contactItem = document.querySelectorAll('.contact-item')
-let contactInfo = document.querySelectorAll('.contact-info')
-let darkMode = localStorage.getItem('darkMode');
-
-
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+let toggleBtn = document.querySelector(".toggle-btn");
+let body = document.querySelector("html");
+let title = document.querySelectorAll(".section-title");
+let p = document.querySelectorAll("p");
+let cta = document.querySelector(".col-right .cta");
+let contactItem = document.querySelectorAll(".contact-item");
+let contactInfo = document.querySelectorAll(".contact-info");
+let darkMode = localStorage.getItem("darkMode");
 
 // Dark mode start //
 
-      //darkMode enable start//
-	  darkModeEnable = () => {
-		toggleBtn.classList.add('active');
-    body.classList.add('active');
-	for (let i = 0; i < title.length; i++) {
-		title[i].classList.add('active')	
-	};
-	for (let i = 0; i < p.length; i++) {
-		p[i].classList.add('active')	
-	};
-	for (let i = 0; i < contactItem.length; i++) {
-		contactItem[i].classList.add('active')	
-	};
-	for (let i = 0; i < contactInfo.length; i++) {
-		contactInfo[i].classList.add('active')	
-	};
-	cta.classList.add('active')
-		
-	localStorage.setItem('darkMode', 'on')
-	  }
+//darkMode enable start//
+darkModeEnable = () => {
+  toggleBtn.classList.add("active");
+  body.classList.add("active");
+  for (let i = 0; i < title.length; i++) {
+    title[i].classList.add("active");
+  }
+  for (let i = 0; i < p.length; i++) {
+    p[i].classList.add("active");
+  }
+  for (let i = 0; i < contactItem.length; i++) {
+    contactItem[i].classList.add("active");
+  }
+  for (let i = 0; i < contactInfo.length; i++) {
+    contactInfo[i].classList.add("active");
+  }
+  cta.classList.add("active");
 
-	//********* **** dark mode enable end // ***********
+  localStorage.setItem("darkMode", "on");
+};
 
-	  // dark mode disable start //
-	  darkModeDisable = () => {
-		toggleBtn.classList.remove('active');
-    body.classList.remove('active');
-	for (let i = 0; i < title.length; i++) {
-		title[i].classList.remove('active')	
-	};
-	for (let i = 0; i < p.length; i++) {
-		p[i].classList.remove('active')	
-	};
-	for (let i = 0; i < contactItem.length; i++) {
-		contactItem[i].classList.remove('active')	
-	};
-	for (let i = 0; i < contactInfo.length; i++) {
-		contactInfo[i].classList.remove('active')	
-	};
-	cta.classList.remove('active')
-		
-	localStorage.setItem('darkMode', 'off')
-	  }  
-	 
-	 
-	  // dark mode disable end //
+//********* **** dark mode enable end // ***********
 
-	// onload condition start //
+// dark mode disable start //
+darkModeDisable = () => {
+  toggleBtn.classList.remove("active");
+  body.classList.remove("active");
+  for (let i = 0; i < title.length; i++) {
+    title[i].classList.remove("active");
+  }
+  for (let i = 0; i < p.length; i++) {
+    p[i].classList.remove("active");
+  }
+  for (let i = 0; i < contactItem.length; i++) {
+    contactItem[i].classList.remove("active");
+  }
+  for (let i = 0; i < contactInfo.length; i++) {
+    contactInfo[i].classList.remove("active");
+  }
+  cta.classList.remove("active");
 
-if (darkMode === 'on') {
-	darkModeEnable()
+  localStorage.setItem("darkMode", "off");
+};
+
+// dark mode disable end //
+
+// onload condition start //
+
+if (darkMode === "on") {
+  darkModeEnable();
 }
 
-	// onload condition end //
+// onload condition end //
 
+//toggle btn click start //
+toggleBtn.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  if (darkMode !== "on") {
+    darkModeEnable();
+  } else {
+    darkModeDisable();
+  }
+});
 
-	//toggle btn click start //
-toggleBtn.addEventListener('click', () => {
-	darkMode = localStorage.getItem('darkMode');
-	if (darkMode !== 'on') {
-		darkModeEnable();
-	
-	} else {
-		darkModeDisable();
-	}
-})
-
-	//toggle btn click end //
+//toggle btn click end //
 
 //**** **/ Dark mode end // **************
 
-
-
 // Hamburger menu start //
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 50) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 50) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
 });
 
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
 
-
-
 // Hamburger menu end //
+
+let year = new Date().getFullYear();
+
+document.querySelector(".copyright").innerHTML = year;
